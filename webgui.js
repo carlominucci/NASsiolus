9b667e897fe8b900ba048a29f3d4f76bec588e17
Carlo Minucci a.k.a. Gecco <gecco@napodano.com>
2019-02-20 19:58:09 +0100

    boh, ho provato a mettere un form ma adesso vado a cena

--- webgui.js
+++ webgui.js
@@ -7,6 +7,8 @@ const options = {
 };
 
 https.createServer(options, (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('hello world\n');
+  	res.writeHead(200, {'Content-Type': 'text/html'});
+  	res.end('<h1>SimpleNASs1</h1>\n');
+	res.end('<input type="text" name="username" /> username<br />\n');
+	res.end('<input type="password" name="password" /> password<br />\n');
 }).listen(8000);
