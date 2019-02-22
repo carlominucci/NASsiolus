21adc737120a06d175fc3225281b775258fb2cf9
Carlo Minucci a.k.a. Gecco <gecco@napodano.com>
2019-02-22 14:10:00 +0100

    dai... sta prendendo forma...

--- webgui.js
+++ webgui.js
@@ -6,9 +6,12 @@ var bodyParser = require("body-parser");
 var port = "11235";
 var os = require('os');
 var path = require('path');
-
 var interfaces = os.networkInterfaces();
 var addresses = [];
+var crypto = require('crypto');
+const headerhtml = fs.readFileSync("./header.html");
+const footerhtml = fs.readFileSync("./footer.html");
+
 for(var k in interfaces){
 	for(var k2 in interfaces[k]){
 		var address = interfaces[k][k2];
@@ -24,19 +27,35 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.set('Content-Type', 'text/html');
-	res.write('<h1>NASsiolus</h1>\n');
+	res.write(headerhtml);
+	res.write('<h1>NASsiolus - ' + os.hostname + ' </h1>\n');
         res.write('<form action="login" method="post">');
-        res.write('<input type="password" name="password" /> password<br />\n');
+        res.write('<input type="password" name="password" />\n');
         res.write('<button>Login</button>\n');
         res.write('</form>\n');
-	res.end();
+	res.end(footerhtml);
 });
 
 app.post('/login', function (req, res){
 	res.set('Content-Type', 'text/html');
+	res.write(headerhtml);
 	var password=req.body.password;
-	res.write(password);
-	res.end();
+	var sha512 = crypto.createHash('sha512').update(password).digest("hex");
+	var contents = fs.readFileSync('./passwd', 'utf8');
+	if(sha512 == contents){
+		res.write('Hostname: ' + os.hostname + '<br />');
+		res.write('Arch: ' + os.arch() + '<br />');
+		res.write('Os: ' + os.type + ' '  + os.platform() + " " + os.release() + '<br />');
+		res.write('Uptime: ' + os.uptime() + '<br />');
+		res.write('Memory: ' + os.totalmem + ' total / ' + os.freemem + ' free<br />');
+		res.write('login ok');
+		var smbconf = fs.readFileSync("/etc/samba/smb.conf", 'utf8');
+		res.write('<pre>' + smbconf + '</pre>');
+	}else{
+		res.write('<h1>password errata</h1><br />');
+		res.write('<a href="/">Torna al Login</a>\n');
+	}
+	res.end(footerhtml);
 });
 
 https.createServer({
@@ -47,3 +66,4 @@ https.createServer({
 .listen(port, function () {
   	console.log("https://" + addresses + ":" + port + "\n");
 });
+
