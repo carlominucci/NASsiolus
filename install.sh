d518aa4065edf7ffacd6baf8bb537baa2c6ecc0a
Carlo Minucci a.k.a. Gecco <gecco@napodano.com>
2019-02-25 14:34:09 +0100

    adesso salva l'smb.conf coi dati giusti e riavvia il servizio

--- install.sh
+++ install.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 echo "Creo i certificati..."
 mkdir /srv/NASsiolus
 openssl genrsa -out /srv/NASsiolus/privatekey.pem 1024
 openssl req -new -key /srv/NASsiolus/privatekey.pem -out /srv/NASsiolus/certrequest.csr
 openssl x509 -req -in /srv/NASsiolus/certrequest.csr -signkey /srv/NASsiolus/privatekey.pem -out /srv/NASsiolus/certificate.pem
 
+echo
 echo "Installo il servizio webgui.js..."
-cp webgui.js /srv/NASsiolus/
-cp /etc/samba/smb.conf /et/samba/smb.conf.orig
+cp -v webgui.js /srv/NASsiolus/
+cp -v header.html /srv/NASsiolus/
+cp -v footer.html /srv/NASsiolus/
+cp -v passwd /srv/NASsiolus/
+cp -v /etc/samba/smb.conf /etc/samba/smb.conf.orig
+mkdir /srv/NASsiolus_share/
 
+echo
 echo "Installo i pacchetti necessari..."
 apk add samba 
 apk add nodejs
 apk add npm
 
+echo
 echo "Installo le dipendenze per nodejs..."
 npm install os
 npm install fs
@@ -22,6 +29,13 @@ npm install body-parser
 npm install crypto
 npm install check-disk-space
 npm install split
+npm install child_process
+npm install express-session
 
 #adduser -s /sbin/nologin -h /dev/null admin 
 #smbpasswd -a admin
+
+echo
+ip address show | grep inet | grep -v inet6 | grep -v 127 | awk {'print "http:", $2'} | sed -e 's/ /\/\//' | sed -e 's/\/24/:11235/'
+echo "Default password is: password";
+echo
--- webgui.js
+++ webgui.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session');
 var fs = require('fs');
 var https = require('https');
 var app = express();
@@ -11,11 +12,20 @@ var addresses = [];
 var crypto = require('crypto');
 var checkDiskSpace = require('check-disk-space');
 var split = require('split');
+var exec = require('child_process').exec;
 var freespace = new String();
 var totalspace = new String();
 var workgroupd = new String();
 var share = new String();
 var user = new String();
+var sambaconfnew;
+
+function poweroff(callback){
+	exec('poweroff', function(error, stdout, stderr){ callback(stdout); });
+}
+function reboot(callback){
+	exec('reboot', function(error, stdout, stderr){ callback(stdout); });
+}
 
 checkDiskSpace('/').then((diskSpace) => {
 	freespace=diskSpace.free;
@@ -56,21 +66,81 @@ app.use(bodyParser.json());
 app.get('/', function (req, res) {
 	res.set('Content-Type', 'text/html');
 	res.write(headerhtml);
-	res.write('<div class="info">\n');
+	res.write('<div class="green">\n');
 	res.write('<h1>NASsiolus - ' + os.hostname + ' </h1>\n');
         res.write('<form action="login" method="post">');
         res.write('<input type="password" name="password" />\n');
         res.write('<button>Login</button>\n');
         res.write('</form>\n</div>\n');
-	res.write('<div class="share">\n');
+	res.write('<div class="red">\n');
 	res.write('smb://' + addresses + '/' + share);
 	res.end(footerhtml);
 });
 
-app.post('/saveshare', function(req, res){
+app.get('/saveshare', function(req, res){
+	response = {
+		workgroup : req.query.workgroup,
+		share : req.query.share,
+		username : req.query.username,
+		pwd : req.query.pwd
+	};
+	sambaconfnew = "[global]\n";
+	sambaconfnew += "workgroup = " + response.workgroup + "\n";
+	sambaconfnew += "server string = " + os.hostname + " " + response.share + " - NASsiolus\n";
+	sambaconfnew += "server role = standalone server\n";
+	sambaconfnew += "log file = /usr/local/samba/var/log.%m\n";
+	sambaconfnew += "max log size = 50\n";
+	sambaconfnew += "[" + response.share + "]\n";
+	sambaconfnew += "comment = " + response.share + " share\n";
+	sambaconfnew += "path = /srv/NASsiolus_share\n";
+	sambaconfnew += "valid users = " + response.username + "\n";
+	sambaconfnew += "public = no\n";
+	sambaconfnew += "writable = yes\n";
+	sambaconfnew += "printable = no\n";
+	sambaconfnew += "create mask = 0765\n";
+	console.log(sambaconfnew);
+	fs.writeFile('/etc/samba/smb.conf', sambaconfnew, function(err){
+		if (err) throw err;
+	});
+	exec("service samba restart");
+	fs.writeFile('/etc/hostname', response.workgroup, function(err){
+		if (err) throw err;
+	});
+	exec("hostname -F /etc/hostname");
+
 	res.set('Content-Type', 'text/html');
-	res.write('ciao');
-	res.end();
+	res.write(headerhtml);
+	res.write('<div class="green">\n');
+	res.write('Salvataggio effettuato.\n');
+	res.write('<form action="/login" method="post">\n');
+	res.write('<button>Back</button>\n');
+	res.write('</form>\n');
+	res.write('</div>\n');
+	res.end(footerhtml);
+});
+
+app.post('/poweroff', function(req, res){
+	res.set('Content-Type', 'text/html');
+	res.write(headerhtml);
+	res.write('<div class="red">\n');
+	res.write('Poweroff in progress...\n');
+	res.write('</div>\n');
+	res.end(footerhtml);
+	poweroff(function(output){
+		console.log(output);
+	});
+});
+
+app.post('/reboot', function(req, res){
+	res.set('Content-Type', 'text/html');
+	res.write(headerhtml);
+	res.write('<div class="red">\n');
+	res.write('Reboot in progress...\n');
+	res.write('</div>\n');
+	res.end(footerhtml);
+	reboot(function(output){
+		console.log(output);
+	});
 });
 
 app.post('/login', function (req, res){
@@ -80,24 +150,42 @@ app.post('/login', function (req, res){
 	var sha512 = crypto.createHash('sha512').update(password).digest("hex");
 	var contents = fs.readFileSync('./passwd', 'utf8');
 
+
 	if(sha512 == contents){
-		res.write('<div class="info">');
+		/*app.set('trust proxy', 1);
+		app.use(session({
+			secret: sha512,
+			resave: false,
+			saveUninitialized: true,
+			cookie: { secure: true, maxAge: 60000}
+		}));
+		console.log(req.session.secret);*/
+		res.write('<div class="red">');
 		res.write('<b> ' + os.hostname + ': </b>' + addresses + '<br />\n');
 		res.write('<b>Os: </b>' + os.type +  ' ' + os.release() + ' ' + os.arch() + '<br />\n');
 		res.write('<b>Uptime: </b>' + os.uptime() + ' s<br />\n');
 		res.write('<b>Memory: </b>' + os.totalmem/1024 + 'Mb total / ' + os.freemem/1024 + 'Mb free<br />\n');
 		res.write('<b>Disk Usage: </b>' + totalspace/1024 + 'Mb total / ' + freespace/1024 + 'Mb free<br />\n');
 		res.write('</div>\n');
-		res.write('<div class="share">\n');
-		res.write('<form action="saveshare" method="post">\n');
+		res.write('<div class="green">\n');
+		res.write('<form action="saveshare" method="get">\n');
 		res.write('<input type="text" name="workgroup" value="' + workgroup + '" /> WorkGroup<br />\n');
 		res.write('<input type="text" name="share" value="' + share + '" /> Share<br />\n');
 		res.write('<input type="text" name="username" value="' + user + '" /> Username<br />\n');
 		res.write('<input type="password" name="pwd" /> Password<br />\n');
-		res.write('<button>Save</button>\n');
+		res.write('<input type="hidden" name="id" value="' + sha512 + '" />\n');
+		res.write('<input type="submit" value="Save" />\n');
 		res.write('</form>\n</div>\n');
+		res.write('<div class="blu">');
+		res.write('<form action="poweroff" method="post">\n');
+		res.write('<button>PowerOff</button>\n');
+		res.write('</form>\n');
+		res.write('<form action="reboot" method="post">\n');
+		res.write('<button>Reboot</button>\n');
+		res.write('</form>\n');
+		res.write('</div>\n');
 	}else{
-		res.write('<div class="share">\n<h1>password errata</h1><br />\n');
+		res.write('<div class="red">\n<h1>password errata</h1><br />\n');
 		res.write('<a href="/">Torna al Login</a>\n</div>\n');
 	}
 	res.end(footerhtml);
